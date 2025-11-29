701f8ef3e606620238b8a3154e7226f5121a7fa2
Cxuan2025 <lmjcpqcpq@gmail.com>
2025-11-28 23:02:59 -0800

    Lesson 5

---
+++ M08.start_a_dialog_workbook/lessons/slideshow_entry.gd
@@ -0,0 +1,4 @@
+class_name SlideShowEntry extends Resource
+@export var expression := preload("res://assets/emotion_regular.png")
+@export var character := preload("res://assets/sophia.png")
+@export_multiline var text := ""
